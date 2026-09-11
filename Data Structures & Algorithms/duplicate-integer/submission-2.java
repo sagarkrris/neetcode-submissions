e2ecee29db636dd9afd5fbedf3b502442f8ba662
Sagar Krishna <sgrkrishna23@gmail.com>
2026-09-11 15:00:15 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+       // if(nums.length<=1) return false;
+        Set<Integer> seen = new HashSet<>();
+        for(int num : nums){
+            if(!seen.add(num)){
+                return true;
+            }
+            
+        }
+        return false;
+    }
+}
